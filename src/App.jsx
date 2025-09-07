f19233a33fefff0598648bd77e2f40e2ba0180fe
diya meghani26 <diyameghani18@gmail.com>
2025-09-07 21:37:23 +0530

    fix: correct Navbar import path

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar"; // ✅ "components" folder name small
+import Navbar from  "./Components/Navbar.jsx"// ✅ "components" folder name small
 import Home from "./pages/Home";
 import Categories from "./pages/categories"; // ✅ file ka naam bhi capital rakho
 import AddItems from "./pages/AddItems";
